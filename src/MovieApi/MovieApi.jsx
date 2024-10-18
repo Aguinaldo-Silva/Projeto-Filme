094544d18cc03975bdc77d1503ba5151c0f79352
Aguinaldo Silva <aguinaldooo.ecio@gmail.com>
2024-10-18 13:14:04 -0300

    adição do api_Key no gitIgnore

--- src/MovieApi/MovieApi.jsx
+++ src/MovieApi/MovieApi.jsx
@@ -1,13 +1,5 @@
-const API_KEY = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NjFjMTFmZGQzNmRjNjNhYmM2MWNlZGJlZDYxNDc4NiIsIm5iZiI6MTcyNDM2OTEzOC44MTA0NDksInN1YiI6IjY2YmU3MTUyOWU0N2I1ZjM4Zjk3OWFjMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9UY0s8o7l425_ipV-zLbSHbuzCEEb1QKJI4cDVWaqpc'
-    }
-};
-const BASE_URL = 'https://api.themoviedb.org/3';
-
-
+import {API_KEY} from './api_key'
+import { BASE_URL } from './api_key'
 
 
 export const getMovies = async () => {
