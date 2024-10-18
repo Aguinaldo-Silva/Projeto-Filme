adacee5c16f6677a7372bf33d0cd80a98e5da9e7
Aguinaldo Silva <aguinaldooo.ecio@gmail.com>
2024-10-18 15:04:39 -0300

    jeito antigo

--- src/MovieApi/api_key.jsx
+++ src/MovieApi/api_key.jsx
@@ -1,9 +1,9 @@
-export  const API_KEY ={
+export const API_KEY = {
     method: 'GET',
     headers: {
         accept: 'application/json',
-        Authorization: `Bearer ${process.env.API_KEY}`
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NjFjMTFmZGQzNmRjNjNhYmM2MWNlZGJlZDYxNDc4NiIsIm5iZiI6MTcyNDM2OTEzOC44MTA0NDksInN1YiI6IjY2YmU3MTUyOWU0N2I1ZjM4Zjk3OWFjMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9UY0s8o7l425_ipV-zLbSHbuzCEEb1QKJI4cDVWaqpc'
     }
 };
-export const BASE_URL = process.env.BASE_URL || 'https://api.themoviedb.org/3';
+export const BASE_URL = 'https://api.themoviedb.org/3';
 
