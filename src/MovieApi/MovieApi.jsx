170d9f26148d8a8315ea1ec435bfb14b512ef0e4
Aguinaldo Silva <aguinaldooo.ecio@gmail.com>
2024-10-18 14:47:57 -0300

    Ajuste na api para poder fazer deploy no vercel

--- src/MovieApi/MovieApi.jsx
+++ src/MovieApi/MovieApi.jsx
@@ -1,12 +1,14 @@
-import {API_KEY} from './api_key'
-import { BASE_URL } from './api_key'
+import { API_KEY, BASE_URL } from './api_key'; 
 
 
 export const getMovies = async () => {
     try {
         const response = await fetch(`${BASE_URL}/discover/movie?include_adult=false&include_video=false&language=pt-BR&page=1&sort_by=popularity.desc`, API_KEY);
+        if (!response.ok) {
+            throw new Error('Erro ao buscar filmes');
+        }
         const data = await response.json();
-        return data.results;
+        return data.results || [];
     } catch (error) {
         console.error('Erro ao buscar filmes:', error);
         return [];
@@ -16,7 +18,10 @@ export const getMovies = async () => {
 
 export const getMovieDetails = async (movieId) => {
     try {
-        const response = await fetch(`${process.env.BASE_URL}/movie/${movieId}?&language=pt-BR`, API_KEY);
+        const response = await fetch(`${BASE_URL}/movie/${movieId}?&language=pt-BR`, API_KEY); 
+        if (!response.ok) {
+            throw new Error('Erro ao buscar detalhes do filme');
+        }
         const data = await response.json();
         return data;
     } catch (error) {
